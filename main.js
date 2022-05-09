385ff93d51df41f185b81167e8b0a62b5a2946aa
matheusmeloni <matheusmelonip@gmail.com>
2022-05-09 00:43:55 -0300

    electron Hello world

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 720
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
